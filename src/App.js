4a682839b6f9802bf45a9798248e08d75a148d7b
Salman-Raza-stack <salmanabid301@gmail.com>
2020-06-30 06:33:17 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import { TextField, Button, CircularProgress } from "@material-ui/core";
+import NavBar from "./NavBar";
+import Post from "./Post";
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    width: 400,
+    margin: "0 auto",
+    textAlign: "center",
+    padding: 15,
+    marginTop: 45,
+  },
+  input: {
+    width: "95%",
+    marginBottom: "10px",
+  },
+}));
+
+export default function SimplePaper() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <NavBar />
+      <Paper elevation={3} className={classes.paper}>
+        <h1>Login</h1>
+
+        <form className={classes.root} novalidate autoComplete="off">
+          <TextField label="Enter Email" className={classes.input} />
+          <TextField label="Enter Password" className={classes.input} />
+          <Button variant="contained" color="primary" className={classes.input}>
+            Login
+          </Button>
+          <CircularProgress value={60} />
+          <Post />
+        </form>
+      </Paper>
+    </div>
+  );
+}
