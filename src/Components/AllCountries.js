4d01941dd891e838433b27d5a244fd868194dcb3
Salman-Raza-stack <salmanabid301@gmail.com>
2020-07-02 05:30:13 +0500

    mistakes solved

--- src/Components/AllCountries.js
+++ src/Components/AllCountries.js
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
-import Grid from "@material-ui/core/Grid";
 
 const useStyles = makeStyles((theme) => ({
   root: {
